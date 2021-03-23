71ceb42844e1bbc6635e39bfcd994916bd99301a
vic tsang <victsang001@gmail.com>
2021-03-23 19:17:18 +0800

    update home title

--- src/core/Home.js
+++ src/core/Home.js
@@ -35,7 +35,7 @@ const Home = () => {
     },[]);
 
     return(
-    <Layout title="Home Page" description="Node React E-commerce app" className="container-fluid">
+    <Layout title="FullStack React Node MongoDB Ecommerce App" description="Node React E-commerce app" className="container-fluid">
             <Search />
             <h2 className="mb-4">New Arrivals</h2>
             <div className="row">
